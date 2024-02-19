6743b3dc736d47ef36c644811a3112c0f122e410
Folarin Folarin <thefolafolarin@gmail.com>
2024-02-19 19:53:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log("The sum of the numbers is:", sum);
+
+let fruits = ["apple", "banana", "orange", "kiwi"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
